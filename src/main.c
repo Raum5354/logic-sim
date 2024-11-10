c49c52e836ea6d9d51a39fcf0de04db6f8abdf59
Raum <raum@raums-macbook-pro-1.home>
2024-11-10 14:06:52 +0100

    Edit comment

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]) {
 	printf("Starting EduLogic: Digital Circuit Simulator\n");
 
-    // Initialize the GUI
+    // Initialise the GUI
     if (initialize_ui() != 0) {
         printf("Failed to initialize UI.\n");
         return 1;
